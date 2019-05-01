bfe566691141d47ca8e8cda69e63f44de215aea0
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-05-01 18:15:08 -0500

    Create class for calculating Distance

---
+++ Project5/src/HammingDist.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HammingDist {
+	protected ArrayList<String> wordBank = new ArrayList<String>();
+	private String word1;
+	private String word2;
+	private int HammDist = 0;
+
+}
