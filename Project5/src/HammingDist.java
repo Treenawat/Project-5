98e101a4385eb5d126e11da1e3a469a1ff4172f9
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-05-01 18:28:03 -0500

    calculate distance

--- Project5/src/HammingDist.java
+++ Project5/src/HammingDist.java
@@ -6,14 +6,12 @@ import java.util.ArrayList;
 
 public class HammingDist {
 	protected ArrayList<String> wordBank = new ArrayList<String>();
-	private String word1;
-	private String word2;
+	private String word;
 	private int HammDist = 0;
 
 	//Constructor reading from Mesonet.txt
-	public HammingDist(String string, String string2) throws IOException{
-		word1 = string;
-		word2 = string2;
+	public HammingDist(String string) throws IOException{
+		word = string;
 		File file= new File("Mesonet.txt");
 		
 		BufferedReader br = new BufferedReader(new FileReader(file));
@@ -29,6 +27,47 @@ public class HammingDist {
         
         br.close();//
 	}
-
+	
+	public void calculate() {
+		//Each Character
+		String outPut = " ";
+		int oneWrong = 0;
+		int twoWrong = 0;
+		int threeWrong = 0;
+		int allWrong = 0;
+		
+		//word
+		for(int i = 0; i < wordBank.size(); i++) {
+			int count = 0;
+			if(!word.substring(0, 1).equals(wordBank.get(i).substring(0, 1))) {
+				count += 1;
+			}
+			
+			if(!word.substring(1, 2).equals(wordBank.get(i).substring(1, 2))){
+				count += 1;
+			}
+			
+			if(!word.substring(2,3).equals(wordBank.get(i).substring(2, 3))) {
+				count += 1;
+			}
+			
+			if(!word.substring(3, 4).equals(wordBank.get(i).substring(3, 4))) {
+				count += 1;
+			}
+			
+			if(count == 1) {
+				oneWrong += 1;
+			}
+			if(count == 2) {
+				twoWrong += 1;
+			}
+			if(count == 3) {
+				threeWrong += 1;
+			}
+			if(count == 4) {
+				allWrong +=1;
+			}
+		}
+	}
 	
 }
