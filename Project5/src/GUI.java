80ad9276bc4235d54a8da1a81e90a60cd5cf0731
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-05-01 21:33:47 -0500

    create GUI class

---
+++ Project5/src/GUI.java
@@ -0,0 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class GUI {
+	public static void main(String[] args) throws IOException {
+		new GUI();
+	}		
+	
+}
